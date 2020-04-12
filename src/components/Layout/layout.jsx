3b55d2d926652671c017d39074f0e29a036a8f03
Johan Oakes <johanoakes@gmail.com>
2020-04-12 14:29:09 -0400

    Use CSS-in-JS, restructure around the new Header, and remove unnecessary parts.

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,32 +1,23 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from '../Header/header';
+import { StyleSheet, css } from 'aphrodite';
+import Header from '../header';
 import './layout.css';
+
+const styles = StyleSheet.create({
+  container: {
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: '0 1.0875rem 1.45rem',
+  },
+});
 
 const Layout = ({ children }) => (
   <>
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0 1.0875rem 1.45rem',
-      }}
-    >
+    <div className={css(styles.container)}>
       <main>{children}</main>
-      <footer>
-        ©
-        {' '}
-        {new Date().getFullYear()}
-        , Johan Oakes
-      </footer>
+      <footer>©{new Date().getFullYear()}, Johan Oakes</footer>
     </div>
   </>
 );
