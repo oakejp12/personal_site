22339c7446cdaf0e31a7cf1ab806c3846acb7868
Johan Oakes <johanoakes@gmail.com>
2020-04-12 14:22:19 -0400

    Remove unnecessary configurations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: 'Johan Oakes',
-    description: 'Personal site of Johan Oakes - includes blogs, links, pictures, etc.',
+    description:
+      'Personal site of Johan Oakes - includes blogs, links, pictures, etc.',
     author: 'Johan Oakes',
-    header: {
-      tabs: ['Blog', 'Github', 'LinkedIn'],
-    }
   },
   plugins: [
     'gatsby-plugin-catch-links',
@@ -19,30 +17,16 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    /* {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    }, */
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
             resolve: 'gatsby-remark-prismjs',
-            options: {
-              inlineCodeMarker: null,
-            },
+            options: {},
           },
         ],
       },
     },
-  ]
+  ],
 };
