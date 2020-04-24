658950598349bb153bb7699c215fffc4879df0c9
Johan Oakes <johanoakes@gmail.com>
2020-04-23 22:46:27 -0400

    Fix grammar and add more spunk.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,11 @@ const Home = () => (
   <Layout>
     <SEO title="Home" />
     <div>
+      <h1>about me</h1>
       <p>
         Costa Rican-born, NYC-based software engineer focusing on algorithms,
         art, and other engineering topics. If you&apos;re interested in what I
-        write and create, click above for a preview of my work. ...
+        write and create, click above for a preview of my work.
       </p>
     </div>
   </Layout>
